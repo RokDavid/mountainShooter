9b2530ae2bcd03eb0db70c2f25c8dcc0036aa352
dawid.rokos@gmail.com <dawid.rokos@gmail.com>
2021-05-09 17:51:06 +0200

    turrets

---
+++ Assets/bulletSpawnPoint.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class bulletSpawnPoint : MonoBehaviour
+{
+
+}
---
+++ Assets/shooter/Code/EnemyController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    Player player;
+    [SerializeField] Bullet bulletPrefab;
+    [SerializeField] Transform bulletSpawnPoint; 
+    [SerializeField, Range(0,100)] float shootDistance = 20;
+    [SerializeField, Range(0.1f, 5f)] float secondsBetweenShoots = 0.5f;
+    [SerializeField] float shootForce = 1000;
+
+    void Awake() {
+        player = FindObjectOfType<Player>();
+        StartCoroutine(ShootCoroutine());
+    }
+
+    IEnumerator ShootCoroutine() {
+        CreateContainerForBullets();
+        while (true) {
+            yield return new WaitForSeconds(secondsBetweenShoots);
+            if (IsPlayerCloseEnough())
+                Shoot();
+        }
+    }
+
+    static void CreateContainerForBullets() {
+        if (GameObject.Find("/Bullets") == null) {
+            var bulltes = Instantiate(new GameObject());
+            bulltes.transform.name = "Bullets";
+        }
+    }
+
+    void Shoot() {
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity, GameObject.Find("/Bullets").transform);
+        
+        var playerDirection = (player.transform.position - bulletSpawnPoint.position).normalized;
+        bullet.GetComponent<Rigidbody>().AddForce(playerDirection * shootForce, ForceMode.VelocityChange);
+    }
+
+    bool IsPlayerCloseEnough() {
+        return Vector3.Distance(player.transform.position, transform.position) < shootDistance;
+    }
+
+    void Update() {
+        transform.LookAt(player.transform.position);
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(transform.position, shootDistance);
+    }
+}
